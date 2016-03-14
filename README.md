81d19f71d3f0b8ded6682f4e74a8654384ece107
Rohul1997 <rohul1997@gmail.com>
2016-03-14 16:51:47 +0000

    Added viewing and editing the Title ID and Unique ID for Exheader and NCCH files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-VoodooChaos is a tool that I wrote that will allow the user to open a CIA file and display the title version and then the user can edit the title version and save it.
+VoodooChaos is a tool that I wrote that will allow the user to open a CIA file and display the title version and then you can edit the title version and save it.
+
+You can open up an NCCH.header file, which will display the Unique ID and the Title ID. You can then edit these information and save it.
+
+You can also open up an Exheader.bin file, which will display the Unique ID and then you can edit and save it.
--- VoodooChaos/Form1.vb
+++ VoodooChaos/Form1.vb
@@ -2,37 +2,144 @@
 
 Public Class Form1
     Dim Open As New OpenFileDialog
-    Dim filepath As String = Open.FileName
-    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-    End Sub
-    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+    Dim filecia As String = Open.FileName
+    Dim filencch As String = Open.FileName
+    Dim fileexheader As String = Open.FileName
+
+    Private Sub Button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Button1.Click
+        'Open up windows file dialog when open cia file button is clicked and wait for users input of cia file. if successful then store the file'
+        Open.Filter = " CIA Files (*.Cia)|*.Cia|All Files (*.*)|*.*"
         If Open.ShowDialog = System.Windows.Forms.DialogResult.OK Then
-            filepath = Open.FileName
-            readfile()
+            filecia = Open.FileName
+            readcia()
         End If
     End Sub
-    Public Sub readfile()
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'Open up windows file dialog when open ncch file button is clicked and wait for users input of ncch file. if successful then store the file'
+        Open.Filter = " NCCH Files (*.header)|*.header|All Files (*.*)|*.*"
+        If Open.ShowDialog = System.Windows.Forms.DialogResult.OK Then
+            filencch = Open.FileName
+            readncch()
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        'Open up windows file dialog when open exheader file button is clicked and wait for users input of exheader file. if successful then store the file'
+        Open.Filter = " Exheader Files (*.bin)|*.bin|All Files (*.*)|*.*"
+        If Open.ShowDialog = System.Windows.Forms.DialogResult.OK Then
+            fileexheader = Open.FileName
+            readexheader()
+        End If
+    End Sub
+
+    Public Sub readcia()
         Try
-            Dim reader As New PackageIO.Reader(filepath, Endian.Little)
-            TextBox2.Enabled = False
+            Dim reader As New PackageIO.Reader(filecia, Endian.Little)
+            'Enable the save cia file button and read the title version value from the cia file and write on to the current title vesion textbox'
             Button2.Enabled = True
             reader.Position = &H2F9C
-            TextBox2.Text = reader.ReadHexString(+2)
+            TextBox1.Text = reader.ReadHexString(2)
+
+            'if something unexpected happens to the file then prompt the following message and disable the save cia file button'
         Catch ex As Exception
-            MessageBox.Show("Invalid .cia")
-            TextBox2.Enabled = False
+            MessageBox.Show("Invalid .cia file")
             Button2.Enabled = False
         End Try
     End Sub
-    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
+    Public Sub readncch()
         Try
-            Dim Writer As New PackageIO.Writer(filepath, Endian.Little)
-            Writer.WriteHexString(TextBox1.Text)
+            Dim reader As New PackageIO.Reader(filencch, Endian.Big)
+            'Enable the save ncch file button and read the unique id value from the ncch file and write on to the current unique id textbox'
+            Button4.Enabled = True
+            reader.Position = &H109
+            TextBox4.Text = reader.ReadHexString(3)
+
+            'Enable the save ncch file button and read the title id value from the ncch file and write on to the current title id textbox'
+            reader.Position = &H150
+            TextBox5.Text = reader.ReadString(10)
+
+            'if something unexpected happens to the file then prompt the following message and disable the save ncch file button'
+        Catch ex As Exception
+            MessageBox.Show("Invalid .header file")
+            Button4.Enabled = False
+        End Try
+    End Sub
+
+    Public Sub readexheader()
+        Try
+            Dim reader As New PackageIO.Reader(fileexheader, Endian.Big)
+            'Enable the save exheader file button and read the unique id value from the ncch file and write on to the current unique id textbox'
+            Button6.Enabled = True
+            reader.Position = &H1C9
+            TextBox8.Text = reader.ReadHexString(3)
+
+            'if something unexpected happens to the file then prompt the following message and disable the save exheader file button'
+        Catch ex As Exception
+            MessageBox.Show("Invalid .bin file")
+            Button6.Enabled = False
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Button2.Click
+        Try
+            Dim Writer As New PackageIO.Writer(filecia, Endian.Little)
+            'when user clicks on the save cia file button then read the text from the new title version box and write it onto the file'
             Writer.Position = &H2F9C
-            Writer.WriteHexString(TextBox1.Text)
-            MsgBox("File Successfully Saved")
+            Writer.WriteHexString(TextBox2.Text)
+
+            'prompt the user that the file has been successfully saved'
+            MsgBox("CIA File Successfully Saved")
+            'prompt the user with the following message if the file couldn't be saved'
         Catch ex As Exception
             MsgBox("Could Not Be Saved", MsgBoxStyle.OkOnly, "Please Try Again Or Try A Diffrent .cia")
+        End Try
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Try
+            Dim Writer As New PackageIO.Writer(filencch, Endian.Big)
+            'when user clicks on the save ncch file button then read the text from the new unique id box and write it onto the file'
+            Writer.Position = &H109
+            Writer.WriteHexString(TextBox3.Text)
+            Writer.Position = &H119
+            Writer.WriteHexString(TextBox3.Text)
+
+        Catch ex As Exception
+            MsgBox("Could Not Be Saved", MsgBoxStyle.OkOnly, "Please Try Again Or Try A Diffrent .header file")
+        End Try
+
+        Try
+            Dim Writer As New PackageIO.Writer(filencch, Endian.Little)
+            'read the text from the new title id box and write it onto the file'
+            Writer.Position = &H150
+            Writer.WriteString(TextBox6.Text)
+
+            'prompt the user that the file has been successfully saved'
+            MsgBox("NCCH File Successfully Saved")
+            'prompt the user with the following message if the file couldn't be saved'
+        Catch ex As Exception
+            MsgBox("Could Not Be Saved", MsgBoxStyle.OkOnly, "Please Try Again Or Try A Diffrent .header file")
+        End Try
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Try
+            Dim Writer As New PackageIO.Writer(fileexheader, Endian.Big)
+            'when user clicks on the save exheader file button then read the text from the new unique id box and write it onto the file'
+            Writer.Position = &H1C9
+            Writer.WriteHexString(TextBox7.Text)
+            Writer.Position = &H201
+            Writer.WriteHexString(TextBox7.Text)
+            Writer.Position = &H230
+            Writer.WriteHexString(TextBox7.Text)
+
+            'prompt the user that the file has been successfully saved'
+            MsgBox("Exheader File Successfully Saved")
+            'prompt the user with the following message if the file couldn't be saved'
+        Catch ex As Exception
+            MsgBox("Could Not Be Saved", MsgBoxStyle.OkOnly, "Please Try Again Or Try A Diffrent .bin file")
         End Try
     End Sub
 
